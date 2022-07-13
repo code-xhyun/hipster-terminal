ac5e3afdea1d6cf61135c0952424dc33e8147e74
SH-Hong <73215772+devShHong@users.noreply.github.com>
2022-07-13 17:45:49 +0900

    Update ascii_banner.sh
    - add lolcat

--- ascii_banner.sh
+++ ascii_banner.sh
@@ -3,9 +3,10 @@
 name=$1
 font=$2
 brew install figlet
+brew install lolcat
 
 if [ $? = 0 ]
 then
-        echo "\nfiglet -w 100 -c -f ${font:-isometric1} $name" >> ~/.zshrc
+        echo "\nfiglet -w 100 -c -f ${font:-isometric1} $name | lolcat" >> ~/.zshrc
         source ~/.zshrc
 fi
